069896221b134942e6e9441186862105a36619c5
Mark Stacey <markjstacey@gmail.com>
2020-04-24 13:25:30 -0300

    Return Promise from `removePermissionsFor` thunk
    `removePermissionsFor` returned a thunk that didn't return a Promise,
    despite doing async work. It now returns a Promise.
    
    The one caller was updated to `await` the result of this operation, as
    as there was a UI state update following the call that seemed like it
    expected the operation to have been completed.

--- ui/app/store/actions.js
+++ ui/app/store/actions.js
@@ -2111,8 +2111,8 @@ export function legacyExposeAccounts (origin, accounts) {
  * Clears the given permissions for the given origin.
  */
 export function removePermissionsFor (domains) {
-  return () => {
-    background.removePermissionsFor(domains)
+  return async () => {
+    await promisifiedBackground.removePermissionsFor(domains)
   }
 }
 
